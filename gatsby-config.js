ef649dc42a27185edbb230beef696ce11e1e4b64
lucas gonçalves <lucasg_f@outlook.com>
2018-04-23 20:05:05 -0300

    alterações no projeto

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,6 +4,7 @@ module.exports = {
   },
   pathPrefix: "/portfolio",
   plugins: [
+    'gatsby-plugin-netlify-cms',
     'gatsby-plugin-react-helmet', {
       resolve: `gatsby-source-filesystem`,
       options: {
